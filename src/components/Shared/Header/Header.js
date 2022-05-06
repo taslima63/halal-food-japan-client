1e616772d20070f0519061cbca8ef5a2da97041a
taslima63 <ruba6315@gmail.com>
2022-05-06 22:15:24 +0900

    navbar and home section added

---
+++ src/components/Shared/Header/Header.js
@@ -0,0 +1,26 @@
+
+import React from 'react';
+import { Container, Nav, Navbar, NavLink } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+
+    return (
+        <>
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" sticky='top'>
+                <Container>
+                    <Navbar.Brand as={Link} to="/"> Halal Food Japan</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+
+                        <Nav>
+                            <Nav.Link as={Link} to='/home' >Home</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </>
+    );
+};
+
+export default Header;
